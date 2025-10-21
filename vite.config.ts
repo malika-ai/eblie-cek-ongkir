fa833de29c6388e33613e7fb92aacf7053dcec5b
Saddan Syah Akbar <73093118+saddansyah@users.noreply.github.com>
2025-10-21 10:16:36 +0700

    fix(vite.config.ts): add base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/eblie-cek-ongkir/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
